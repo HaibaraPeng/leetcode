dc84069e14035ad7edc3cb5b43d3aa72b7efb0e7
dongp <dongp@laschain.com>
2022-05-12 11:15:03 +0800

    N叉树的层序遍历

--- src/main/java/com/roc/leetcode/editor/cn/ConstructQuadTree.java
+++ src/main/java/com/roc/leetcode/editor/cn/ConstructQuadTree.java
@@ -103,53 +103,52 @@
  */
 package com.roc.leetcode.editor.cn;
 
-import com.roc.leetcode.editor.cn.common.Node;
-
 public class ConstructQuadTree {
     public static void main(String[] args) {
         Solution solution = new ConstructQuadTree().new Solution();
     }
+
+    /**
+     * Definition for a QuadTree node.
+     */
+    class Node {
+        public boolean val;
+        public boolean isLeaf;
+        public Node topLeft;
+        public Node topRight;
+        public Node bottomLeft;
+        public Node bottomRight;
+
+
+        public Node() {
+            this.val = false;
+            this.isLeaf = false;
+            this.topLeft = null;
+            this.topRight = null;
+            this.bottomLeft = null;
+            this.bottomRight = null;
+        }
+
+        public Node(boolean val, boolean isLeaf) {
+            this.val = val;
+            this.isLeaf = isLeaf;
+            this.topLeft = null;
+            this.topRight = null;
+            this.bottomLeft = null;
+            this.bottomRight = null;
+        }
+
+        public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+            this.val = val;
+            this.isLeaf = isLeaf;
+            this.topLeft = topLeft;
+            this.topRight = topRight;
+            this.bottomLeft = bottomLeft;
+            this.bottomRight = bottomRight;
+        }
+    }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-/*
-// Definition for a QuadTree node.
-class Node {
-    public boolean val;
-    public boolean isLeaf;
-    public Node topLeft;
-    public Node topRight;
-    public Node bottomLeft;
-    public Node bottomRight;
-
-    
-    public Node() {
-        this.val = false;
-        this.isLeaf = false;
-        this.topLeft = null;
-        this.topRight = null;
-        this.bottomLeft = null;
-        this.bottomRight = null;
-    }
-    
-    public Node(boolean val, boolean isLeaf) {
-        this.val = val;
-        this.isLeaf = isLeaf;
-        this.topLeft = null;
-        this.topRight = null;
-        this.bottomLeft = null;
-        this.bottomRight = null;
-    }
-    
-    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
-        this.val = val;
-        this.isLeaf = isLeaf;
-        this.topLeft = topLeft;
-        this.topRight = topRight;
-        this.bottomLeft = bottomLeft;
-        this.bottomRight = bottomRight;
-    }
-};
-*/
-
     class Solution {
         public Node construct(int[][] grid) {
             return null;
