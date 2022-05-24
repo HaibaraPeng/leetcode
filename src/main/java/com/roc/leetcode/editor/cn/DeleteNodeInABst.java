6dd62a5c4aec9104a51ed805ffe46c5360774c69
dongp <dongp@laschain.com>
2022-05-24 11:25:59 +0800

    删除二叉树中的节点

---
+++ src/main/java/com/roc/leetcode/editor/cn/DeleteNodeInABst.java
@@ -0,0 +1,168 @@
+//给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的
+//根节点的引用。 
+//
+// 一般来说，删除节点可分为两个步骤： 
+//
+// 
+// 首先找到需要删除的节点； 
+// 如果找到了，删除它。 
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入：root = [5,3,6,2,4,null,7], key = 3
+//输出：[5,4,6,2,null,null,7]
+//解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+//一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+//另一个正确答案是 [5,2,6,null,4,null,7]。
+//
+//
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: root = [5,3,6,2,4,null,7], key = 0
+//输出: [5,3,6,2,4,null,7]
+//解释: 二叉树不包含值为 0 的节点
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: root = [], key = 0
+//输出: [] 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 节点数的范围 [0, 10⁴]. 
+// -10⁵ <= Node.val <= 10⁵ 
+// 节点值唯一 
+// root 是合法的二叉搜索树 
+// -10⁵ <= key <= 10⁵ 
+// 
+//
+// 
+//
+// 进阶： 要求算法时间复杂度为 O(h)，h 为树的高度。 
+// Related Topics 树 二叉搜索树 二叉树 👍 762 👎 0
+
+/**
+ * @author dongp
+ */
+package com.roc.leetcode.editor.cn;
+
+public class DeleteNodeInABst {
+    public static void main(String[] args) {
+        Solution solution = new DeleteNodeInABst().new Solution();
+        //输入：root = [5,3,6,2,4,null,7], key = 3
+//输出：[5,4,6,2,null,null,7]
+        //输入: root = [5,3,6,2,4,null,7], key = 0
+//输出: [5,3,6,2,4,null,7]
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(7);
+        solution.deleteNode(root, 3);
+        root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(7);
+        solution.deleteNode(root, 0);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public TreeNode deleteNode(TreeNode root, int key) {
+            if (root == null) {
+                return null;
+            } else if (root.val == key && root.right == null) {
+                return root.left;
+            } else if (root.val == key && root.right.left == null) {
+                root.right.left = root.left;
+                return root.right;
+            } else if (root.val == key) {
+                TreeNode tempPre = root.right;
+                TreeNode temp = root.right.left;
+                while (temp.left != null) {
+                    tempPre = temp;
+                    temp = temp.left;
+                }
+                tempPre.left = deleteNode(temp, temp.val);
+                temp.left = root.left;
+                temp.right = root.right;
+                return temp;
+            } else if (root.val < key) {
+                deleteNode(root, root.right, key, false);
+            } else {
+                deleteNode(root, root.left, key, true);
+            }
+            return root;
+        }
+
+        private void deleteNode(TreeNode preNode, TreeNode node, int key, boolean isLeft) {
+            if (node == null) {
+                return;
+            }
+            if (node.val > key) {
+                deleteNode(node, node.left, key, true);
+            } else if (node.val < key) {
+                deleteNode(node, node.right, key, false);
+            } else {
+                if (isLeft) {
+                    preNode.left = deleteNode(node, node.val);
+                } else {
+                    preNode.right = deleteNode(node, node.val);
+                }
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
