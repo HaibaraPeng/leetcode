62b1f32b3ed811ad67615157c02ef2068bcaaafc
dongp <dongp@laschain.com>
2022-08-31 11:30:11 +0800

    第一个错误的版本

--- src/main/java/com/roc/leetcode/editor/cn/FirstBadVersion.java
+++ src/main/java/com/roc/leetcode/editor/cn/FirstBadVersion.java
@@ -42,29 +42,29 @@ package com.roc.leetcode.editor.cn;
 
 public class FirstBadVersion {
     public static void main(String[] args) {
-        Solution solution = new FirstBadVersion().new Solution();
+//        Solution solution = new FirstBadVersion().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
-    public class Solution extends VersionControl {
-        public int firstBadVersion(int n) {
-            int left = 0, right = n;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (isBadVersion(mid)) {
-                    right = mid;
-                    if (left == right) {
-                        return right;
-                    }
-                } else {
-                    left = mid + 1;
-                }
-            }
-            return -1;
-        }
-    }
+//    public class Solution extends VersionControl {
+//        public int firstBadVersion(int n) {
+//            int left = 0, right = n;
+//            while (left <= right) {
+//                int mid = left + (right - left) / 2;
+//                if (isBadVersion(mid)) {
+//                    right = mid;
+//                    if (left == right) {
+//                        return right;
+//                    }
+//                } else {
+//                    left = mid + 1;
+//                }
+//            }
+//            return -1;
+//        }
+//    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
