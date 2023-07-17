0e12d9354785caa16920c07aa24c40b1fb163604
Roc <dongpeng@aeke.com>
2023-07-17 17:40:42 +0800

    无重复字符的最长子串

---
+++ src/main/java/com/roc/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,79 @@
+//给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 5 * 10⁴ 
+// s 由英文字母、数字、符号和空格组成 
+// 
+//
+// Related Topics 哈希表 字符串 滑动窗口 👍 9360 👎 0
+
+/**
+ * @author dongp
+ */
+package com.roc.leetcode.editor.cn;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+        Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
+        solution.lengthOfLongestSubstring("abcabcbb");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            if (s == null || s.length() == 0) {
+                return 0;
+            }
+            HashSet<Character> set = new HashSet<>();
+            int left = 0, res = 0;
+            for (int right = 0; right < s.length(); right++) {
+                Character character = s.charAt(right);
+                if (set.contains(character)) {
+                    res = Math.max(res, set.size());
+                    while (set.contains(character)) {
+                        set.remove(s.charAt(left));
+                        left++;
+                    }
+                }
+                set.add(character);
+            }
+            res = Math.max(res, set.size());
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
